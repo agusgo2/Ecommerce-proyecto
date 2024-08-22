cf3d36f84efacffe2f8f952268e4c9b075cef617
agusgo2 <agusgo182@gmail.com>
2024-08-21 23:54:06 -0300

    Dropmenu version 1

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,14 @@ function NavBar() {
       <h1>Mi E-Commerce</h1>
       <ul>
         <li><a href="">Inicio</a></li>
-        <li><a href="">Productos</a></li>
+        <li className="dropdown">
+          <a href="">Productos</a>
+          <div className="dropdown-content">
+            <a href="">Categoría 1</a>
+            <a href="">Categoría 2</a>
+            <a href="">Categoría 3</a>
+          </div>
+        </li>
         <li><a href="">Contacto</a></li>
       </ul>
 
